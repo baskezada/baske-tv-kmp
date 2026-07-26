7a89f8383b85cd7c43aae6e71e3ac8ace8ae67b4
Bastian Quezada <baskezada@gmail.com>
2026-07-26 19:36:45 -0400

    Override R8 to 9.1.31 to parse Kotlin 2.4.10 metadata (fixes R8 warning)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // R8 más nuevo que el que trae AGP 9.0.1, para que parsee la metadata de
+        // Kotlin 2.4.10 (elimina el warning "error parsing kotlin metadata" en R8).
+        classpath("com.android.tools:r8:9.1.31")
+    }
+}
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
